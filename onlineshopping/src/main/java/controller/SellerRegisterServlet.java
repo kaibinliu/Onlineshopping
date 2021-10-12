6a28e1b53c080670b788f911f1f40fe16b2276d3
kaibinliu <83156792+kaibinliu@users.noreply.github.com>
2021-10-12 22:46:46 +0800

    卖家登陆注册功能的数据处理工作转移
    由Servlet中移动到新建立的SellerLoginDao和SellerRegisterDao中

--- onlineshopping/src/main/java/controller/SellerRegisterServlet.java
+++ onlineshopping/src/main/java/controller/SellerRegisterServlet.java
@@ -20,6 +20,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
+import model.Seller;
+import model.SellerRegisterDao;
+
 
 
 
@@ -36,9 +39,6 @@ public class SellerRegisterServlet extends HttpServlet {
      */
     public SellerRegisterServlet() {
         super();
-        try {  Class.forName("com.mysql.cj.jdbc.Driver");
-	      }
-	      catch(Exception e){} 
         // TODO Auto-generated constructor stub
     }
 
@@ -47,40 +47,19 @@ public class SellerRegisterServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String url = "jdbc:mysql://127.0.0.1:3306/onlineshopping?useSSL=false&serverTimezone=UTC&characterEncoding=utf-8";
-		String username="root";
-		String password="001124";
-		try {
-				Connection con = DriverManager.getConnection(url,username,password);
-				String insertSQL = "insert into seller values(?,?)";
-				String querySQL = "select * from seller where SUsername=?";
-				PreparedStatement pre = con.prepareStatement(querySQL, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-				request.setCharacterEncoding("utf-8");
-				String SUsername=request.getParameter("uname");
-				String SPassword=request.getParameter("pwd");
-				pre.setString(1, SUsername);
-				ResultSet rs = pre.executeQuery();
-				rs.last();
-				int n = rs.getRow();
-				if(n>0) {
-					RequestDispatcher dispatcher=request.getRequestDispatcher("SellerRegister_fail.jsp");
-        			dispatcher.forward(request,response);
-					con.close();
-				}else {
-					pre = con.prepareStatement(insertSQL);
-					pre.setString(1, SUsername);
-					pre.setString(2, SPassword);
-					int ok = pre.executeUpdate();
-					con.close();
-					RequestDispatcher dispatcher=request.getRequestDispatcher("SellerRegister_success.jsp");
-        			dispatcher.forward(request,response);
-				}
-			} catch (SQLException e) {
-				// TODO 自动生成的 catch 块
-				e.printStackTrace();
-				RequestDispatcher dispatcher=request.getRequestDispatcher("SellerRegister_fail.jsp");
-    			dispatcher.forward(request,response);
-			}	
+		request.setCharacterEncoding("utf-8");
+		String SUsername=request.getParameter("uname");
+		String SPassword=request.getParameter("pwd");
+		Seller seller = new Seller(SUsername,SPassword);
+		SellerRegisterDao sellerRD = new SellerRegisterDao();
+		boolean a = sellerRD.register(seller);
+		if(a) {
+			RequestDispatcher dispatcher=request.getRequestDispatcher("SellerRegister_success.jsp");
+			dispatcher.forward(request,response);
+		}else {
+			RequestDispatcher dispatcher=request.getRequestDispatcher("SellerRegister_fail.jsp");
+			dispatcher.forward(request,response);
+		}
 	}
 
 	/**
---
+++ onlineshopping/src/main/java/model/SellerRegisterDao.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.servlet.RequestDispatcher;
+
+public class SellerRegisterDao {
+	String url = "jdbc:mysql://127.0.0.1:3306/onlineshopping?useSSL=false&serverTimezone=UTC&characterEncoding=utf-8";
+	String username="root";
+	String password="001124";
+	
+	public boolean register(Seller seller) {
+		try {  Class.forName("com.mysql.cj.jdbc.Driver");
+	      }
+	      catch(Exception e){}
+		try {
+			Connection con = DriverManager.getConnection(url,username,password);
+			String insertSQL = "insert into seller values(?,?)";
+			String querySQL = "select * from seller where SUsername=?";
+			PreparedStatement pre = con.prepareStatement(querySQL, ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+			pre.setString(1, seller.getUname());
+			ResultSet rs = pre.executeQuery();
+			rs.last();
+			int n = rs.getRow();
+			if(n>0) {
+				con.close();
+				return false;
+			}else {
+				pre = con.prepareStatement(insertSQL);
+				pre.setString(1, seller.getUname());
+				pre.setString(2, seller.getPwd());
+				int ok = pre.executeUpdate();
+				con.close();
+				return true;
+			}
+		} catch (SQLException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+			return false;
+		}	
+	}
+}
